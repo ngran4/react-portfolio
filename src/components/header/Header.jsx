95f9d1f381905d1b297c6d42d0e4264cd5f49508
ngran4 <nicgranville@gmail.com>
2023-01-10 16:52:41 -0800

    style header, link socials and resume

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Nicole Granville</h1>
+        <h5 className="text-light">Fullstack Engineer</h5>
+        <CTA />
+        <HeaderSocials/>
+
+        <div>
+          <img src={ME} alt="me" className='me'/>
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
