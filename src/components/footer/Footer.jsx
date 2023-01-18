a6baafb77bfd70d66610f4e9d444d8fe50baccf4
ngran4 <nicgranville@gmail.com>
2023-01-18 10:43:11 -0800

    add portfolio to nav

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,15 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portoflio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </footer>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,7 +3,7 @@ import './nav.css'
 import {AiOutlineHome} from 'react-icons/ai'
 import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
+import {VscFolderLibrary} from 'react-icons/vsc'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 import {useState} from 'react'
 
@@ -16,7 +16,7 @@ const Nav = () => {
       onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
-      {/* <a href="#services" onClick={() => setActiveNav('#skills')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a> */}
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><VscFolderLibrary/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
