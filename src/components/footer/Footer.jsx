52c6f141809dfb3f883ccbdabe324696f61d18b3
ngran4 <nicgranville@gmail.com>
2023-01-18 13:04:23 -0800

    change color scheme

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ import { FaGithub } from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>N L G</a>
+      <a href="#" className='footer__logo'>NLG</a>
 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <CTA />
         <HeaderSocials/>
 
-        <div>
+        <div >
           <img src={ME} alt="me" className='me'/>
         </div>
 
