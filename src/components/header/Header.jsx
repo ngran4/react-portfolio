6f87215af74908cb4bdaf60acc9bec22e436508a
ngran4 <nicgranville@gmail.com>
2023-01-18 16:12:02 -0800

    add smaller media query for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,13 +10,13 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Nicole Granville</h1>
-        <h5 className="text-light">Fullstack Engineer</h5>
+        <h5 className="text-light">Fullstack Software Engineer</h5>
         <CTA />
         <HeaderSocials/>
 
-        <div >
-          <img src={ME} alt="me" className='me'/>
-        </div>
+        
+        <img src={ME} alt="me" className='me'/>
+        
 
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
