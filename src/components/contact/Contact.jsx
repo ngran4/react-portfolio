d90558f72711fc69b720ba4d93c12960713d742f
ngran4 <nicgranville@gmail.com>
2023-07-07 11:56:28 -0700

    finish styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
-          <button type='submit' className='btn btn-primary montserrat'>Send Message</button>
+          <button type='submit' className='btn btn-primary btn-contact montserrat'>Send Message</button>
         </form>
       </div>
 
