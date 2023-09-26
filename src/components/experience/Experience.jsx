e3babc8abdde288b2fd4b59cea6f2a33f4c41816
ngran4 <nicgranville@gmail.com>
2023-09-26 13:17:56 -0700

    edit responsiveness

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -44,7 +44,7 @@ const Experience = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>Mantine</h4>
+                <h4>Next.js</h4>
                 <small className='text-light'>Basic</small>
               </div>
             </article>
